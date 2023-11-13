adbb57f2e6090e7cc4c38db4f8f29f7994fd51c3
Eichi-Emu <maturiku2002@gmail.com>
2023-11-13 15:13:08 +0900

    add-switch sentenses

---
+++ parts_filter.php
@@ -0,0 +1,76 @@
+<?php
+
+function parts_filter($genre)
+{
+    switch ($genre) {
+        case "cpu":
+            $str = <<<DOM
+
+            DOM;
+            echo $str;
+            break;
+
+        case "cpuc":
+            $str = <<<DOM
+
+            DOM;
+            echo $str;
+            break;
+
+        case "ram":
+            $str = <<<DOM
+
+            DOM;
+            echo $str;
+            break;
+
+        case "mb":
+            $str = <<<DOM
+    
+            DOM;
+            echo $str;
+            break;
+
+        case "gpu":
+            $str = <<<DOM
+    
+            DOM;
+            echo $str;
+            break;
+
+        case "ssd":
+            $str = <<<DOM
+
+            DOM;
+            echo $str;
+            break;
+
+        case "ssd2":
+            $str = <<<DOM
+    
+            DOM;
+            echo $str;
+            break;
+
+        case "hdd":
+            $str = <<<DOM
+    
+            DOM;
+            echo $str;
+            break;
+
+        case "psu":
+            $str = <<<DOM
+    
+            DOM;
+            echo $str;
+            break;
+
+        case "pccase":
+            $str = <<<DOM
+    
+            DOM;
+            echo $str;
+            break;
+    }
+}
