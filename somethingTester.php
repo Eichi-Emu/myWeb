3d0505a485c481ab8d3afe42618b61ff48c70a11
Eichi-Emu <maturiku2002@gmail.com>
2024-05-14 17:30:37 +0900

    change tester.

--- somethingTester.php
+++ somethingTester.php
@@ -5,7 +5,8 @@
 <?
 include_once "Query_Encoder.php";
 echo"よめとるぞ";
-$test_str = "cpu=3&cpuc=1&ram=1&mb=1&gpu=1&ssd2=1&hdd=9&psu=5&pccase=12&os=10";
+$test_str = "os=1&cpuc=1&mb=1&ssd=2";
+//$test_str = "cpu=3&cpuc=1&ram=1&mb=1&gpu=1&ssd2=1&hdd=9&psu=5&pccase=12&os=10";
 $test_pcs_array = [1,1,1,1,1,1,1,1,1,1,1];
 
 parse_str($test_str,$test_array);
